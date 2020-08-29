4efee17e44500363a46d7c398b9e9f6d2869937c
Chuian <chuiantw1212@gmail.com>
2020-08-29 17:49:18 +0800

    update layouts

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,24 @@
 <template>
-    <div class="container">
+    <div class="container py-5">
         <div class="row">
-            <div class="col-md-8">
-                <h1 class="my-4">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
-                <Post></Post>
-                <Post></Post>
-                <Post></Post>
-                <Post></Post>
+            <!-- <div class="col"></div> -->
+            <div class="col-6">
                 <Post></Post>
             </div>
-            <!-- Sidebar Widgets Column -->
-            <div class="col-md-4">
-                <WidgetSearch></WidgetSearch>
-                <WidgetCategories></WidgetCategories>
+            <div class="col-6">
+                <Post></Post>
+            </div>
+            <div class="col-6">
+                <Post></Post>
+            </div>
+            <div class="col-6">
+                <Post></Post>
+            </div>
+            <div class="col-6">
+                <Post></Post>
+            </div>
+            <div class="col-6">
+                <Post></Post>
             </div>
         </div>
         <!-- /.row -->
