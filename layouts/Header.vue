a40bef77ea3aeb49ead5ed4f0fea9925037f7c69
Chuian <chuiantw1212@gmail.com>
2020-08-29 22:26:15 +0800

    update layouts

--- layouts/Header.vue
+++ layouts/Header.vue
@@ -1,8 +1,8 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="/">常識經濟學</a>
-            <button
+    <nav class="navbar navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="/">常識經濟學</a>
+        <!-- <div class="container">
+             <button
                 class="navbar-toggler"
                 type="button"
                 data-toggle="collapse"
@@ -12,20 +12,20 @@
                 aria-label="Toggle navigation"
             >
                 <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
+        </button>-->
+        <!-- <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="#">金融</a>
                     </li>
-                    <!-- <li class="nav-item">
+                    <li class="nav-item">
                         <a class="nav-link" href="#">房地產</a>
-                    </li> -->
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">創業</a>
                     </li>
                 </ul>
             </div>
-        </div>
+        </div>-->
     </nav>
 </template>
