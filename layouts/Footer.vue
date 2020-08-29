b70460a797b18027f401e2126d09f74055d05858
Chuian <chuiantw1212@gmail.com>
2020-08-29 22:31:17 +0800

    update footer

--- layouts/Footer.vue
+++ layouts/Footer.vue
@@ -1,9 +1,20 @@
 <template>
     <div class="footer">
-        <span class="footer__logo">常識經濟學</span>
+        <div class="footer__logoWrapper">
+            <span @click="routeToHome()" class="footer__logo">常識經濟學</span>
+        </div>
         <span class="footer__info">© 2020 常識經濟學 只教你需要的</span>
     </div>
 </template>
+<script>
+export default {
+    methods: {
+        routeToHome() {
+            this.$router.push('/')
+        }
+    }
+}
+</script>
 <style lang="scss" scoped>
 .footer {
     background-color: #5d5a51;
@@ -12,17 +23,25 @@
     height: auto;
     padding: 0;
     position: relative;
-    text-align: center;
+    // text-align: center;
     padding: 2rem;
     color: white;
-    .footer__logo {
-        display: block;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    .footer__logoWrapper {
         text-align: center;
-        color: #fff;
-        font-size: 32px;
-        font-weight: 100;
-        letter-spacing: 2px;
-        text-decoration: none;
+        display: flex;
+        justify-content: center;
+        .footer__logo {
+            display: block;
+            color: #fff;
+            font-size: 32px;
+            font-weight: 100;
+            letter-spacing: 2px;
+            text-decoration: none;
+            cursor: pointer;
+        }
     }
     .footer__info {
         text-align: center;
