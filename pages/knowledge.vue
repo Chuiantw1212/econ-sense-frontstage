04a0f5362733342db5deb1798e06b476ad4a80c9
Chuian <chuiantw1212@gmail.com>
2020-11-10 21:19:47 +0800

    add knowledge

--- pages/knowledge.vue
+++ pages/knowledge.vue
@@ -1,9 +1,87 @@
 <template>
-    <div class="container text-center py-5">
-        <h1>主動投資人生</h1>
-        <p class="m-0">心思，用來讓自己成為更好的人吧</p>
-        <p class="m-0">這裡要放東吳大學進修部 為你而讀 讀冊生活的連結</p>
-        <p class="m-0">可能還會有大人學</p>
-        <p class="m-0">賺錢，也賺幸福這本書的內容簡介</p>
-    </div>
+	<div class="container text-center py-5">
+		<h1>主動投資人生</h1>
+		<p class="m-0">心思，用來讓自己成為更好的人吧</p>
+		<br />
+		<h2>東吳大學推廣部</h2>
+		<p class="m-0">
+			個人非常推薦
+			<a href="https://www.ext.scu.edu.tw/" target="_blank"> 東吳大學推廣部 </a>
+			的進修課程
+		</p>
+		<p class="m-0">時數長，手把手教</p>
+		<p class="m-0">費用實惠</p>
+		<p class="m-0">
+			交通
+			<span class="text-danger">非常</span>
+			方便
+		</p>
+		<p class="m-0">從西門捷運站2出口只要走6分鐘就到了</p>
+		<br />
+		<p class="m-0">而且是名門學校開班授課</p>
+		<p class="m-0">完全不用擔心老師會為了賺錢教一些奇怪的東西</p>
+		<p class="m-0">或是上完課馬上推銷下一門原價8888特價7878的課程</p>
+		<br />
+		<p class="m-0">我的第一堂理財課</p>
+		<p class="m-0">上的是彭老師的如何做好個人理財規劃</p>
+		<p class="m-0">很感謝他教學很多很正統的東西</p>
+		<p class="m-0">算是為初學理財的我打好了基礎</p>
+		<a href="https://www.ext.scu.edu.tw/" target="_blank">
+			<img
+				src="https://storage.googleapis.com/my-blog-287510.appspot.com/knowledge/suchow.jpg"
+				alt="東吳大學進修部"
+			/>
+		</a>
+		<h2 class="mt-5">讀冊生活</h2>
+		<p class="m-0">讀冊生活是我除了誠品以外最常用的購書管道</p>
+		<p class="m-0">
+			除了可以販賣
+			<del>不要的書</del>二手書以外
+		</p>
+		<p class="m-0">最重要的是可以找到已絕版的書</p>
+		<p class="m-0">剛開始是為了找到本書的前身"錢與閒"</p>
+		<p class="m-0">才無意中發現這個平台已經默默成立很久了</p>
+		<br />
+		<a
+			href="https://www.taaze.tw/apredir.html?154858690/https://www.taaze.tw/goods/11100899131.html?"
+			target="_blank"
+		>
+			<img
+				src="https://storage.googleapis.com/my-blog-287510.appspot.com/knowledge/tazze.jpg"
+			/>
+		</a>
+		<br />
+		<h2 class="mt-5">賺錢，也賺幸福</h2>
+		<p class="m-0">整個網站的思想都是延伸自這本書的內容</p>
+		<br />
+		<p class="m-0">我剛開始接觸股票投資的時候</p>
+		<p class="m-0">第一年也根本戒不掉每小時看一次K線的習慣</p>
+		<p class="m-0">投資能穩下心態，是很多閱讀與實踐累積的成果</p>
+		<p class="m-0">之所以讀推這本，是因為這本書涵蓋了最多的內容</p>
+		<p class="m-0">大多數的人，賺錢是為了生活</p>
+		<p class="m-0">投資理財也是為了更好的生活</p>
+		<br />
+		<p class="m-0">但好多人卻寧願購買高價的投資課程</p>
+		<p class="m-0">去隨著市場起伏心驚膽跳、寢食難安</p>
+		<p class="m-0">為了那1%的報酬率賠掉大半的精力(還有錢!)</p>
+		<br />
+		<!-- <p class="m-0">賺錢，也賺幸福這本書的內容簡介</p> -->
+		<p class="m-0">這本書除了談到投資的方法</p>
+		<p class="m-0">也涵蓋了工作、稅務、保險等面相</p>
+		<p class="m-0">最重要的是不斷提醒，讓所有行為為心中的核心價值服務</p>
+		<a
+			href="https://www.taaze.tw/apredir.html?154858690/https://www.taaze.tw/goods/11100899131.html?"
+			target="_blank"
+		>
+			<p class="m-0">擁抱幸福的生活</p>
+		</a>
+		<a
+			href="https://www.taaze.tw/apredir.html?154858690/https://www.taaze.tw/goods/11100899131.html?"
+			target="_blank"
+		>
+			<img
+				src="https://storage.googleapis.com/my-blog-287510.appspot.com/knowledge/The%20Millionaire%20in%20you%20.jpg"
+			/>
+		</a>
+	</div>
 </template>
