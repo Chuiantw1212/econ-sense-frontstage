a67ac2cb96f43ec0109902f10468689c71fe78e5
Ian <chuiantw1212@gmail.com>
2021-02-10 21:41:39 +0800

    update self

--- pages/self.vue
+++ pages/self.vue
@@ -5,20 +5,7 @@
             截斷一切偷心，自然正念常存。譬如滴水穿石，何愁入道無門。
         </p>
         <hr />
-        <h2>前言</h2>
-        <p>底下是一個真實故事</p>
-        <div class="row justify-content-center">
-            <ul class="text-left">
-                <li>國中資優班，校內排名20</li>
-                <li>國高中連續六年，沒日沒夜補習</li>
-                <li>大學差點21，一度有憂鬱症症狀</li>
-            </ul>
-        </div>
-        <p>希望這短短三行敘述，足以描述一個連續10年失速下墜的軌跡</p>
-        <p>我完全能理解什麼是"無助"，也更能體會何謂逆境中才有覺察</p>
-        <p>以下是我盤點找到自己步調的因素</p>
-        <br />
-        <h3>找對老師與學習方法</h3>
+        <h2>1. 找對老師與學習方法</h2>
         <img
             class="d-block m-auto"
             src="https://storage.googleapis.com/my-blog-287510.appspot.com/DISC.png"
@@ -31,25 +18,34 @@
             同時一個人的原型人格跟他終生體會的精髓通常是完全相反的
         </p>
         <br />
-        <h4>Domainance支配型</h4>
-        <p class="m-0">原型："給我說重點"、"我得找人來做這件事情"</p>
-        <p class="m-0">成長："魔鬼藏在細節裡"</p>
-        <br />
-        <h4>Influence影響型</h4>
-        <p class="m-0">原型："人脈就是錢脈"</p>
-        <p class="m-0">
-            成長："交朋友的成本超乎你的想像"、"別讓無效社交害了你"
-        </p>
-        <br />
-        <h4>Steadiness穩定型</h4>
-        <p class="m-0">
-            原型："你好我好大家好"、"熬久了就是你的了"、"穩定很重要"
-        </p>
-        <p class="m-0">成長："你的善良必須有點鋒芒"</p>
-        <br />
-        <h4>Consciencetiousness分析型</h4>
-        <p class="m-0">原型："Talk is cheap, show me the data."</p>
-        <p class="m-0">成長："不會說話很吃虧"</p>
+        <div class="row">
+            <div class="col-12 col-md-6 border p-2">
+                <h4>Domainance支配型</h4>
+                <p class="m-0">原型："給我說重點"、"我得找人來做這件事情"</p>
+                <p class="m-0">成長："魔鬼藏在細節裡"</p>
+            </div>
+            <div class="col-12 col-md-6 border p-2">
+                <h4>Influence影響型</h4>
+                <p class="m-0">原型："人脈就是錢脈"</p>
+                <p class="m-0">
+                    成長："交朋友的成本超乎你的想像"、"別讓無效社交害了你"
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-6 border p-2">
+                <h4>Steadiness穩定型</h4>
+                <p class="m-0">
+                    原型："你好我好大家好"、"熬久了就是你的了"、"和諧很重要"
+                </p>
+                <p class="m-0">成長："你的善良必須有點鋒芒"</p>
+            </div>
+            <div class="col-12 col-md-6 border p-2">
+                <h4>Consciencetiousness分析型</h4>
+                <p class="m-0">原型："Talk is cheap, show me the data."</p>
+                <p class="m-0">成長："不會說話很吃虧"</p>
+            </div>
+        </div>
         <br />
         <p class="m-0">
             許多家庭在教育上的衝突，正來自於都想把自己畢生體會的"真理"
@@ -68,8 +64,134 @@
         <br />
 
         <br />
-        <h3>學以致用</h3>
-        <h2>賺錢，也賺幸福</h2>
+        <h2>2. 持續精進與學習</h2>
+        <p class="m-0">
+            比起氾濫且以行銷目的居多的的線上影音，我比較推線下的教學
+        </p>
+        <p class="m-0">互動更頻繁，也能結交人脈</p>
+        <p class="m-0">能力有所增長，也才能驅動OPT, OPM, OPR</p>
+        <br />
+        <div class="row">
+            <div class="card col-12 col-lg-4">
+                <a
+                    class="card-img-top"
+                    rel="noreferrer"
+                    href="https://www.ext.scu.edu.tw/"
+                    target="_blank"
+                >
+                    <img
+                        src="https://storage.googleapis.com/my-blog-287510.appspot.com/knowledge/suchow.jpg"
+                        alt="東吳大學進修部"
+                    />
+                </a>
+                <div class="card-body">
+                    <h5 class="card-title">東吳大學推廣部</h5>
+                    <p class="card-text">時數長，手把手教，費用實惠</p>
+                    <p>
+                        交通
+                        <span class="text-danger">非常</span>
+                        方便
+                    </p>
+                    <p>從西門捷運站2出口只要走6分鐘就到了</p>
+                </div>
+            </div>
+            <div class="card col-12 col-lg-4">
+                <a
+                    class="card-img-top"
+                    rel="noreferrer"
+                    href="https://www.accupass.com/"
+                    target="_blank"
+                >
+                    <img
+                        class="col-8 col-lg-12"
+                        src="https://storage.googleapis.com/my-blog-287510.appspot.com/knowledge/social_media_accupass_logo.jpg"
+                        alt="活動通"
+                    />
+                </a>
+                <div class="card-body">
+                    <h5 class="card-title">活動通</h5>
+                    <p class="card-text">
+                        我在
+                        <a
+                            rel="noreferrer"
+                            href="https://www.accupass.com/"
+                            target="_blank"
+                            >活動通</a
+                        >
+                        參加上百場活動了
+                    </p>
+                    <p>從對理財一竅不通到能夠出來<del>傳教</del>分享</p>
+                    <p>超過一半的心路歷程是跟Accupass有關</p>
+                    <p>絕對推薦給學習慾望強烈想拓展自己的人</p>
+                </div>
+            </div>
+            <div class="card col-12 col-lg-4">
+                <div class="card-body">
+                    <a
+                        class="card-img-top"
+                        rel="noreferrer"
+                        href="https://www.kscthinktank.com.tw/"
+                        target="_blank"
+                    >
+                        <img
+                            class="d-block m-auto"
+                            src="https://storage.googleapis.com/my-blog-287510.appspot.com/knowledge/readforyou.png"
+                            alt="為你而讀"
+                        />
+                    </a>
+                    <br />
+                    <h5 class="card-title">為你而讀</h5>
+                    <p>主要為自我成長等偏商業的主題</p>
+                    <p>每個月都會固定開班，內容CP值破表</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="card col-12 col-lg-4">
+                <div class="card-body">
+                    <a
+                        class="card-img-top"
+                        rel="noreferrer"
+                        href="https://www.darencademy.com/course"
+                        target="_blank"
+                    >
+                        <img
+                            class="d-block m-auto"
+                            src="https://storage.googleapis.com/my-blog-287510.appspot.com/knowledge/daren.png"
+                            alt="大人學"
+                        />
+                    </a>
+                    <br />
+                    <h5 class="card-title">大人學</h5>
+                    <p>除了有PMI PDU相關的課程以外</p>
+                    <p>也有一些買車買房結婚之類等</p>
+                    <p>名符其實的大人必學的課題</p>
+                </div>
+            </div>
+            <div class="card col-12 col-lg-4">
+                <div class="card-body">
+                    <a
+                        class="card-img-top"
+                        rel="noreferrer"
+                        href="https://www.taaze.tw/apredir.html?154858690/https://www.taaze.tw/goods/11100899131.html?"
+                        target="_blank"
+                    >
+                        <img
+                            class="d-block m-auto"
+                            src="https://storage.googleapis.com/my-blog-287510.appspot.com/knowledge/tazze.jpg"
+                            alt="讀冊生活"
+                        />
+                    </a>
+                    <br />
+                    <h5 class="card-title">讀冊生活</h5>
+                    <p>可以購買二手書籍</p>
+                    <p>也可以賣二手書籍</p>
+                    <p>讓家中不要的藏書可以發揮效用</p>
+                </div>
+            </div>
+        </div>
+        <br />
+        <h2>3. 在財富與幸福間取得平衡</h2>
         <p class="m-0">
             整個網站的思想都是延伸自
             <a
@@ -119,131 +241,5 @@
         </div>
         <hr />
         <br />
-        <h2>進修學習資源</h2>
-        <p class="m-0">
-            比起氾濫且以行銷目的居多的的線上影音，我比較推線下的教學
-        </p>
-        <p class="m-0">互動更頻繁，也能結交人脈</p>
-        <br />
-        <div class="row mt-5">
-            <div class="col-12 col-lg-4 mb-5">
-                <h3>東吳大學推廣部</h3>
-                <p class="m-0">
-                    非常推薦
-                    <a
-                        rel="noreferrer"
-                        href="https://www.ext.scu.edu.tw/"
-                        target="_blank"
-                    >
-                        東吳大學推廣部
-                    </a>
-                    的進修課程
-                </p>
-                <p class="m-0">時數長，手把手教</p>
-                <p class="m-0">費用實惠</p>
-                <p class="m-0">
-                    交通
-                    <span class="text-danger">非常</span>
-                    方便
-                </p>
-                <p class="m-0">從西門捷運站2出口只要走6分鐘就到了</p>
-                <br />
-                <p class="m-0">而且是名門學校開班授課</p>
-                <p class="m-0">完全不用擔心老師會為了賺錢教一些奇怪的東西</p>
-                <p class="m-0">或是推銷下一門原價8888特價7878的課程</p>
-                <br />
-                <a
-                    rel="noreferrer"
-                    href="https://www.ext.scu.edu.tw/"
-                    target="_blank"
-                >
-                    <img
-                        src="https://storage.googleapis.com/my-blog-287510.appspot.com/knowledge/suchow.jpg"
-                        alt="東吳大學進修部"
-                    />
-                </a>
-            </div>
-            <div class="col-12 col-lg-4 mb-5">
-                <h3>Accupass活動通</h3>
-                <p class="m-0">
-                    我在
-                    <a
-                        rel="noreferrer"
-                        href="https://www.accupass.com/"
-                        target="_blank"
-                        >活動通</a
-                    >
-                    參加上百場活動了
-                </p>
-                <p class="m-0">從對理財一竅不通到能夠出來<del>傳教</del>分享</p>
-                <p class="m-0">超過一半的心路歷程是跟Accupass有關</p>
-                <p class="m-0">絕對推薦給學習慾望強烈想拓展自己的人</p>
-                <br />
-                <a
-                    rel="noreferrer"
-                    href="https://www.accupass.com/"
-                    target="_blank"
-                >
-                    <img
-                        class="col-8 col-lg-12"
-                        src="https://storage.googleapis.com/my-blog-287510.appspot.com/knowledge/social_media_accupass_logo.jpg"
-                        alt="活動通"
-                    />
-                </a>
-            </div>
-            <div class="col-12 col-lg-4 mb-5">
-                <h3>為你而讀</h3>
-                <p class="m-0">為你而讀的創辦人</p>
-                <p class="m-0">最剛開始見到他是在臺大某研討會</p>
-                <p class="m-0">除了多樣化的說書以外，也舉辦知性聯誼活動</p>
-                <p class="m-0">不過近期因為拓展線上業務</p>
-                <p class="m-0">活動多以企業導向居多</p>
-                <br />
-                <a
-                    rel="noreferrer"
-                    href="https://www.kscthinktank.com.tw/"
-                    target="_blank"
-                >
-                    <img
-                        src="https://storage.googleapis.com/my-blog-287510.appspot.com/knowledge/readforyou.png"
-                        alt="為你而讀"
-                    />
-                </a>
-            </div>
-        </div>
-        <br />
-        <div class="row">
-            <div class="col-12 col-lg-4 mb-5">
-                <h3>讀冊生活</h3>
-                <p class="m-0">
-                    <a
-                        rel="noreferrer"
-                        href="https://www.taaze.tw/apredir.html?154858690/https://www.taaze.tw/goods/11100899131.html?"
-                        target="_blank"
-                    >
-                        讀冊生活
-                    </a>
-                    是我除了誠品以外最常用的購書管道
-                </p>
-                <p class="m-0">
-                    除了可以販賣
-                    <del>不要的書</del>二手書以外
-                </p>
-                <p class="m-0">最重要的是可以找到已絕版的書</p>
-                <p class="m-0">剛開始是為了找到"錢與閒"這本書</p>
-                <p class="m-0">才無意中發現這個平台已經默默成立很久了</p>
-                <br />
-                <a
-                    rel="noreferrer"
-                    href="https://www.taaze.tw/apredir.html?154858690/https://www.taaze.tw/goods/11100899131.html?"
-                    target="_blank"
-                >
-                    <img
-                        src="https://storage.googleapis.com/my-blog-287510.appspot.com/knowledge/tazze.jpg"
-                        alt="讀冊生活"
-                    />
-                </a>
-            </div>
-        </div>
     </div>
 </template>
