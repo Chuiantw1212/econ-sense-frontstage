cec59aa5cfffe20ad176bf344ae8f8117824dbf5
Ian <chuiantw1212@gmail.com>
2021-02-12 21:49:13 +0800

    update reources

--- pages/resources.vue
+++ pages/resources.vue
@@ -5,9 +5,103 @@
             斷捨離的主角並不是物品，而是自己，而時間軸永遠都是現在
         </p>
         <hr />
-        <p>工事中</p>
-        <!-- <h2>精準消費</h2>
+        <h2>耐用的消費</h2>
+        <p>選擇保固期最久的，其次是最讓自己心動的</p>
+        <p>有關耐久財與品牌保固的關聯，請點下方按鈕</p>
+        <div class="row justify-content-center">
+            <button
+                class="btn btn-primary"
+                type="button"
+                data-toggle="collapse"
+                data-target="#collapseExample"
+                aria-expanded="false"
+                aria-controls="collapseExample"
+            >
+                展開相關列表
+            </button>
+        </div>
+        <br />
+        <div class="collapse" id="collapseExample">
+            <div class="card card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">品項</th>
+                            <th scope="col">推薦廠牌</th>
+                            <th scope="col">保固年期</th>
+                            <th scope="col">備註</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">車子</th>
+                            <td>Toyota</td>
+                            <td>
+                                <a
+                                    href="https://www.toyota.com.tw/owner_guarantee.aspx"
+                                    target="_blank"
+                                >
+                                    4年或120,000公里
+                                </a>
+                            </td>
+                            <td>
+                                <a
+                                    href="https://caredge.com/ranks/depreciation/popular/7-year/best"
+                                    >折舊率也非常優異</a
+                                >
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">家具與家居用品</th>
+                            <td>IKEA</td>
+                            <td>
+                                <a
+                                    href="https://www.ikea.com.tw/zh/customerservice/about-product/guarantees-and-warranties"
+                                    target="_blank"
+                                >
+                                    3~25年不等
+                                </a>
+                            </td>
+                            <td>兼具功能與美觀的超強家具商</td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">家電</th>
+                            <td>Panasonic</td>
+                            <td>
+                                <p class="m-0">冰箱：重點零件3年</p>
+                                <p class="m-0">冷暖氣：7年</p>
+                                <p class="m-0">電視：3年</p>
+                            </td>
+                            <td>
+                                <a
+                                    href="https://pmst.panasonic.com.tw/service/guarExtHelp.aspx#:~:text=2015%2F02%2F01(%E5%90%AB,%E6%A9%9F%E3%80%8D%E4%BA%AB7%E5%B9%B4%E4%BF%9D%E8%AD%89%E3%80%82"
+                                    target="_blank"
+                                >
+                                    整體表現最優異
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <br />
+        <p>沒有保固期的物品(比如衣服)依據幾個原則去購買</p>
+        <div class="row justify-content-center">
+            <div class="col col-lg-8 text-left">
+                <ul>
+                    <li>
+                        定量：不是衣櫃永遠少一件這麼浪漫的事情，是根本從來沒有想過什麼品項會讓自己心動，定量就是一進一出限制收藏數量，強迫自己思考自己要什麼
+                    </li>
+                    <li>定型：統一一個風格元素，比如V領、單肩等等</li>
+                    <li>定色：統一顏色</li>
+                </ul>
+            </div>
+        </div>
+        <p></p>
+        <p></p>
         <h2>斷捨離</h2>
-        <h2>定期檢視</h2> -->
+        <h2>定期檢視</h2>
     </div>
 </template>
