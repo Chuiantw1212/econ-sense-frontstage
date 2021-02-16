9616ca0f61dea9b1d8a2e771bce93c7c90e4dfbb
Ian <chuiantw1212@gmail.com>
2021-02-16 22:32:14 +0800

    update resources

--- pages/resources.vue
+++ pages/resources.vue
@@ -2,7 +2,7 @@
     <div class="container text-center py-5">
         <h1>資源盤點</h1>
         <p class="m-0">
-            斷捨離的主角並不是物品，而是自己，而時間軸永遠都是現在
+            時間、金錢、人際關係
         </p>
         <hr />
         <h2>耐用的消費</h2>
@@ -70,7 +70,7 @@
                             <td>Panasonic</td>
                             <td>
                                 <p class="m-0">冰箱：重點零件3年</p>
-                                <p class="m-0">冷暖氣：7年</p>
+                                <p class="m-0">冷、暖氣：7年</p>
                                 <p class="m-0">電視：3年</p>
                             </td>
                             <td>
@@ -78,7 +78,7 @@
                                     href="https://pmst.panasonic.com.tw/service/guarExtHelp.aspx#:~:text=2015%2F02%2F01(%E5%90%AB,%E6%A9%9F%E3%80%8D%E4%BA%AB7%E5%B9%B4%E4%BF%9D%E8%AD%89%E3%80%82"
                                     target="_blank"
                                 >
-                                    整體表現最優異
+                                    整體保固最優異
                                 </a>
                             </td>
                         </tr>
@@ -103,5 +103,42 @@
         <p></p>
         <h2>斷捨離</h2>
         <h2>定期檢視</h2>
+        <p>有意識的檢視時間、金錢、人脈</p>
+        <!-- <p class="m-0">金融科技與法規的進步</p>
+        <p class="m-0">讓不動手就能記帳這件事情成為現實</p>
+        <a
+            rel="noreferrer"
+            href="https://moneybook.com.tw/?referrer=CQxbIc"
+            target="_blank"
+        >
+            <img
+                src="https://storage.googleapis.com/my-blog-287510.appspot.com/flow/moneybook.jpg"
+                alt="麻布記帳"
+            />
+        </a>
+        <p class="m-0">
+            <a
+                rel="noreferrer"
+                href="https://moneybook.com.tw/?referrer=CQxbIc"
+                target="_blank"
+                >麻布記帳</a
+            >綁定網路銀行權限，自動同步更新所有收支，從此不再需要紀錄發票
+        </p>
+        <p class="m-0">這樣的金融服務，每個月月費要收多少呢</p>
+        <br />
+        <p class="m-0 text-danger">免費</p>
+        <br />
+        <p class="m-0">
+            透過
+            <a
+                rel="noreferrer"
+                href="https://moneybook.com.tw/?referrer=CQxbIc"
+                target="_blank"
+                >連結</a
+            >
+            取得15天免費同步
+        </p>
+        <p class="m-0">15天過後也只是換成要手動去按某一顆按鈕同步而已</p>
+        <p class="m-0">功能依舊強大，而且依舊免費</p> -->
     </div>
 </template>
