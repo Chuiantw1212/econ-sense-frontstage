10261aa5fd42181d84abe5c6d728e7e9dac530f7
Ian Chu <chuiantw1212@gmail.com>
2022-10-09 22:49:29 +0800

    add back

--- pages/pension.vue
+++ pages/pension.vue
@@ -4,53 +4,20 @@
         <p class="m-0">不算不知道，一算嚇一跳。</p>
         <i>— 某不知名網頁工程師　朱老闆</i>
         <hr />
-        <div class="row justify-content-center">
-            <div class="col-12 col-md-8 col-lg-6">
-                <h2>中華民國退休基金協會試算</h2>
-                <p>點擊圖片開始試算</p>
-                <a href="https://rfa.pension.org.tw/RetireComputer-Person/startpage.php" target="_blank">
-                    <img src="@/assets/pension.png"
-                /></a>
-            </div>
-        </div>
-        <!-- <div class="row justify-content-center mt-5">
+        <div class="row justify-content-center mt-5">
             <div class="col-12 col-md-8 col-lg-6">
                 <h2>勞退專戶試算</h2>
-                <InputNumber
-                    v-model="monthlyIncome"
-                    title="個人目前薪資（元/每月）"
-                    @change="updateChart()"
-                ></InputNumber>
-                <InputNumber
-                    :value="getEstimatedIncomeTax()"
-                    title="預估每年綜所稅"
-                    :disabled="true"
-                    @change="updateChart()"
-                ></InputNumber>
-                <InputNumber
-                    v-model="age"
-                    title="勞退提繳起算年齡"
-                    @change="updateCareerLength(), updateChart()"
-                ></InputNumber>
-                <InputNumber
-                    v-model="retireYear"
-                    title="欲申請退休之年齡（至少60）"
-                    @change="updateCareerLength(), updateChart()"
-                ></InputNumber>
-                <InputNumber
-                    v-model="careerLength"
-                    title="預估勞退年資（年）"
-                    disabled
-                    @change="updateChart()"
-                ></InputNumber>
+                <InputNumber v-model="monthlyIncome" title="個人目前薪資（元/每月）" @change="updateChart()"></InputNumber>
+                <InputNumber :value="getEstimatedIncomeTax()" title="預估每年綜所稅" :disabled="true" @change="updateChart()">
+                </InputNumber>
+                <InputNumber v-model="age" title="勞退提繳起算年齡" @change="updateCareerLength(), updateChart()"></InputNumber>
+                <InputNumber v-model="retireYear" title="欲申請退休之年齡（至少60）" @change="updateCareerLength(), updateChart()">
+                </InputNumber>
+                <InputNumber v-model="careerLength" title="預估勞退年資（年）" disabled @change="updateChart()"></InputNumber>
                 <InputNumber v-model="internalRateReturn" title="預估個人退休金投資報酬率（%/每年）"></InputNumber>
                 <InputNumber value="6" title="雇主提撥6%" disabled></InputNumber>
-                <InputNumber
-                    v-model="depositRateMannul"
-                    title="自行提撥率（最多自提6%）"
-                    :maximumValue="6"
-                    @change="updateChart()"
-                ></InputNumber>
+                <InputNumber v-model="depositRateMannul" title="自行提撥率（最多自提6%）" :maximumValue="6"
+                    @change="updateChart()"></InputNumber>
                 <p class="text-left">累積退休金：{{ getAccountValue().toLocaleString() }}元</p>
                 <p class="text-left">累積節稅效益:{{ getTaxDeduction() }}</p>
                 <canvas id="pensionAccount"></canvas>
@@ -95,31 +62,40 @@
                     <li>綜所稅以單身試算扣除一般扣除額120,000，另外僅扣除薪資特別扣除額</li>
                     <li>
                         每月經常薪資：2021年的國人薪資中位數約莫為500,000元，換算每月的經常性薪資約莫是42000左右。
-                        最新資訊可以從<a href="https://earnings.dgbas.gov.tw/experience_sub_01.aspx" target="_blank"
-                            >行政院主計總處</a
-                        >取得。
+                        最新資訊可以從<a href="https://earnings.dgbas.gov.tw/experience_sub_01.aspx"
+                            target="_blank">行政院主計總處</a>取得。
                     </li>
                     <li>
-                        退休年齡：2021年的國人預期退休年齡中位數約為65歲。且餘命參考自<a
-                            href="https://www.bli.gov.tw/0018437.html"
-                            target="_blank"
-                            >勞退試算</a
-                        >
+                        退休年齡：2021年的國人預期退休年齡中位數約為65歲。且餘命參考自<a href="https://www.bli.gov.tw/0018437.html"
+                            target="_blank">勞退試算</a>
                     </li>
                     <li>
                         預估個人退休金投資報酬率：4.62%，引用自勞動部2021年3月的<a
                             href="https://www.mol.gov.tw/topic/6025/13123/34950/#:~:text=%E6%88%AA%E8%87%B3109%E5%B9%B412%E6%9C%88,%E6%94%B6%E7%9B%8A%E7%8E%874.62%EF%BC%85%E3%80%82"
-                            target="_blank"
-                            >經營績效</a
-                        >。
+                            target="_blank">經營績效</a>。
                     </li>
                 </ul>
             </div>
         </div>
-        <img
-            class="container__image"
-            src="https://storage.googleapis.com/my-blog-287510.appspot.com/pension/middle%20salary.png"
-        /> -->
+        <img class="container__image"
+            src="https://storage.googleapis.com/my-blog-287510.appspot.com/pension/middle%20salary.png" />
+        <!-- <div class="row justify-content-center text-left">
+            <div class="col-12 col-md-8 col-lg-6">
+                <h2>最低儲蓄率計算機</h2>
+                <h3>主計處統計薪資</h3>
+                <InputRadio v-model="education" name="教育程度" :items="pensionItems"></InputRadio>
+            </div>
+        </div> -->
+        <hr />
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-8 col-lg-6">
+                <h2>中華民國退休基金協會試算</h2>
+                <p>點擊圖片開始試算</p>
+                <a href="https://rfa.pension.org.tw/RetireComputer-Person/startpage.php" target="_blank">
+                    <img src="@/assets/pension.png" /></a>
+            </div>
+        </div>
+
     </div>
 </template>
 <script>
@@ -131,6 +107,21 @@ export default {
     },
     data: function () {
         return {
+            education: null,
+            pensionItems: [
+                {
+                    text: '學士',
+                    value: 'bachelor'
+                },
+                {
+                    text: '碩士',
+                    value: 'master'
+                },
+            ],
+            incomeData: {
+                bachelor: ['47.9', '55.9', '74.8', '81.4'],
+                master: ['68.8', '90.2', '120.2', '129.2']
+            },
             age: 40,
             monthlyIncome: 41500,
             internalRateReturn: 4.62,
@@ -152,9 +143,9 @@ export default {
         InputNumber: () => import('@/components/input/InputNumber.vue')
     },
     mounted() {
-        // this.careerLength = Math.max(0, this.retireYear - this.age)
-        // this.initChart()
-        // this.updateChart()
+        this.careerLength = Math.max(0, this.retireYear - this.age)
+        this.initChart()
+        this.updateChart()
     },
     methods: {
         getTaxDeduction() {
@@ -206,9 +197,6 @@ export default {
         getLifeRemain() {
             const retireYear = Math.max(60, this.retireYear)
             const lifeRemain = lifeExpectancy[retireYear]
-            console.log({
-                lifeRemain
-            })
             return lifeRemain
         },
         updateCareerLength() {
--- plugins/main.js
+++ plugins/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 // inputs
-import InputRadios from '@/components/input/InputRadios.vue'
-Vue.component('InputRadios', InputRadios)
+// import InputRadios from '@/components/input/InputRadios.vue'
+// Vue.component('InputRadios', InputRadios)
+import InputRadio from '@/components/atoms/InputRadio.vue'
+Vue.component('InputRadio', InputRadio)
 import InputNumber from '@/components/input/InputNumber.vue'
 Vue.component('InputNumber', InputNumber)
 import InputText from '@/components/input/InputText.vue'
