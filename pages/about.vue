5e8cd3319b33e83c59536bb1ecbc6ef866a49849
Chuian <chuiantw1212@gmail.com>
2023-09-25 06:24:56 +0800

    Revert "wip"
    This reverts commit aed936da12fbc74678a55273e0914f220a115ace.

--- pages/about.vue
+++ pages/about.vue
@@ -19,6 +19,395 @@
         finance management and full-stack developing.
       </p>
     </div>
+    <hr />
+    <section>
+      <h2>里程碑：職缺媒合平台</h2>
+      <div class="row">
+        <div class="col">
+          <div class="card mb-4 box-shadow">
+            <!-- <div class="card__background card__background--mbia">
+
+                        </div> -->
+            <img src="https://storage.googleapis.com/public.prd.job-pair.com/meta/ogImage.png" />
+            <div class="card-body">
+              <h5 class="card-title">
+                <a href="https://job-pair-dev.web.app" target="_blank">Job Pair</a>
+                <!-- <a href="https://www.mbia.com.tw/" target="_blank"> 倉儲管理公司(2021) </a> -->
+              </h5>
+              <p class="card-text">
+                創辦人：
+                <a href="https://www.facebook.com/3dlifecheck" target="_blank">
+                  生涯設計師Sandy </a>，台灣生涯設計工作坊創辦人
+              </p>
+            </div>
+          </div>
+        </div>
+        <!-- <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <div class="card__background card__background--josie"></div>
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <a href="https://chuiantw1212.github.io/josies-portfolio/#/home" target="_blank">
+                                    UIUX設計師個人網頁(2019)
+                                </a>
+                            </h5>
+                            <p class="card-text">協助珠寶設計師轉職網站設計師的網頁</p>
+                        </div>
+                    </div>
+                </div> -->
+      </div>
+      <!-- <iframe
+                src="https://docs.google.com/presentation/d/e/2PACX-1vSVpXaRcurZ3ER0imCM1XtSj6_0Nk8j9EvotPY1dTsXBYKLta-FXkkRGp9DcXb3KfsHSUwTu_9N8w4Y/embed?start=false&loop=false&delayms=3000"
+                frameborder="0"
+                width="100%"
+                height="569"
+                allowfullscreen="true"
+                mozallowfullscreen="true"
+                webkitallowfullscreen="true"
+            ></iframe> -->
+      <!-- <div class="row">
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <div class="card__background card__background--mbia"></div>
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <a href="https://www.mbia.com.tw/" target="_blank"> 倉儲管理公司(2021) </a>
+                            </h5>
+                            <p class="card-text">使用Nuxt製作，Alpha中</p>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+    </section>
+    <hr />
+    <!-- <section>
+            <h2>外包專案</h2>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <div class="card__background card__background--mbia"></div>
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <a href="https://www.mbia.com.tw/" target="_blank"> 倉儲管理公司(2021) </a>
+                            </h5>
+                            <p class="card-text">使用Nuxt製作</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <div class="card__background card__background--josie"></div>
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <a href="https://chuiantw1212.github.io/josies-portfolio/#/home" target="_blank">
+                                    UIUX設計師個人網頁(2019)
+                                </a>
+                            </h5>
+                            <p class="card-text">協助珠寶設計師轉職網站設計師的網頁</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section> -->
+    <section>
+      <h2>核心能力</h2>
+      <div class="row">
+        <div class="col-md-6 mt-3">
+          <div class="card">
+            <div class="card-body">
+              <h4>網頁製作</h4>
+              <ul>
+                <li>Vue.js(Nuxt) + Fastify.js + Firebase + GCP</li>
+                <li>獨立全端開發Job Pair求職平台</li>
+                <li>
+                  <a href="https://photos.app.goo.gl/q1xuAmBce8o1EJQZ7" target="_blank">
+                    Javscript/Node.js社群管理員
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 mt-3">
+          <div class="card">
+            <div class="card-body">
+              <h4>金融理財</h4>
+              <ul>
+                <li>興趣使然的理財規劃顧問 (AFP)</li>
+                <li>持有多張金融證照</li>
+                <li>主要投資ETF、太陽能板</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 mt-3">
+          <div class="card">
+            <div class="card-body">
+              <h4>
+                <a>教學分享</a>
+              </h4>
+              <ul>
+                <li>
+                  常識經濟學：
+                  <a href="https://www.instagram.com/econ.sense/" target="_blank">
+                    Instagram </a>、<a href="https://www.facebook.com/econ.sense.tw" target="_blank">FB粉專</a>
+                </li>
+                <li>誠遠國際合作理財講師</li>
+                <li>
+                  <a href="https://photos.app.goo.gl/WTnP5qWKmStsjoW27" target="_blank">
+                    不定期舉辦講座與分享
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 mt-3">
+          <div class="card">
+            <div class="card-body">
+              <h4>專案管理</h4>
+              <ul>
+                <li>留美主攻 Project Management</li>
+                <li>2020/05 4A成績正式取得PMP證照</li>
+                <li>2021/02 Job Pair協辦人</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="mt-5">
+      <h2>證照與證明</h2>
+      <div class="row text-center">
+        <div class="col-md-6 mt-3">
+          <div class="card">
+            <div class="card-body">
+              <h4>PMP</h4>
+              <img class="image--full-width"
+                src="@/assets/AFPI2300016_U121652155-1.png" />
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 mt-3">
+          <div class="card">
+            <div class="card-body">
+              <h4>PMP</h4>
+              <img class="image--full-width"
+                src="https://storage.googleapis.com/my-blog-287510.appspot.com/about/pmp.jpg" />
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 mt-3">
+          <div class="card">
+            <div class="card-body">
+              <h4>Northeastern</h4>
+              <img class="image--full-width"
+                src="https://storage.googleapis.com/my-blog-287510.appspot.com/about/northeastern.jpg" />
+            </div>
+          </div>
+        </div>
+        <!-- 直版 -->
+        <div class="col-6 col-md-3 mt-3">
+          <div class="card">
+            <div class="card-body">
+              <h6>理財規劃人員</h6>
+              <img class="image--full-width"
+                src="https://storage.googleapis.com/my-blog-287510.appspot.com/about/financeManagement.jpeg" />
+            </div>
+          </div>
+        </div>
+        <div class="col-6 col-md-3 mt-3">
+          <div class="card">
+            <div class="card-body">
+              <h6>金融科技力</h6>
+              <img class="image--full-width"
+                src="https://storage.googleapis.com/my-blog-287510.appspot.com/about/fin-tech.JPG" />
+            </div>
+          </div>
+        </div>
+        <div class="col-6 col-md-3 mt-3">
+          <div class="card">
+            <div class="card-body">
+              <h6>信託業業務人員</h6>
+              <img class="image--full-width"
+                src="https://storage.googleapis.com/my-blog-287510.appspot.com/about/Proficiency%20Test%20for%20Trust%20Operations%20Personnel.JPG" />
+            </div>
+          </div>
+        </div>
+        <div class="col-6 col-md-3 mt-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h6>金融市場常識與職業道德</h6>
+                            <img
+                                class="image--full-width"
+                                src="https://storage.googleapis.com/my-blog-287510.appspot.com/about/financeSense.JPG"
+                            />
+                        </div>
+                    </div>
+                </div>
+        <div class="col-6 col-md-3 mt-3">
+          <div class="card">
+            <div class="card-body">
+              <h6>投信投顧相關法規</h6>
+              <img class="image--full-width"
+                src="https://storage.googleapis.com/my-blog-287510.appspot.com/about/Investment%20Trust%20and%20Consulting%20Regulations.JPG" />
+            </div>
+          </div>
+        </div>
+        <!-- <div class="col-6 col-md-3 mt-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h6>鵝堡初階桌遊師</h6>
+                            <img
+                                class="image--full-width"
+                                src="https://storage.googleapis.com/my-blog-287510.appspot.com/about/teacher-junior.JPG"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-md-3 mt-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h6>鵝堡進階桌遊師</h6>
+                            <img
+                                class="image--full-width"
+                                src="https://storage.googleapis.com/my-blog-287510.appspot.com/about/teacher-senior.JPG"
+                            />
+                        </div>
+                    </div>
+                </div> -->
+        <!-- <div class="col-6 col-md-4 mt-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h6>給非行銷人員的第一堂行銷課</h6>
+                            <img
+                                class="image--full-width"
+                                src="https://storage.googleapis.com/my-blog-287510.appspot.com/about/marketingManagement.jpeg"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-md-4 mt-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h6>電腦玩物Esor的時間管理哲學</h6>
+                            <img
+                                class="image--full-width"
+                                src="https://storage.googleapis.com/my-blog-287510.appspot.com/about/marketingManagement.jpeg"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-md-4 mt-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h6>302專案的談判與協商</h6>
+                            <img
+                                class="image--full-width"
+                                src="https://storage.googleapis.com/my-blog-287510.appspot.com/about/negotiation-and-mediation-in-a-proejct-environment.JPG"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-md-4 mt-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h6>P046軟體產業才能掌握的敏捷式團隊工作術</h6>
+                            <img
+                                class="image--full-width"
+                                src="https://storage.googleapis.com/my-blog-287510.appspot.com/about/learning-agile-and%20-scrum-through-games.JPG"
+                            />
+                        </div>
+                    </div>
+                </div> -->
+      </div>
+    </section>
+    <section class="mt-5">
+      <h2>學經歷</h2>
+      <MyListItem>
+        <h4>Job Pair創業專案 全端職涯里程碑</h4>
+        <h5>February 2021 ~</h5>
+        <ul>
+          <li>前端：Vue3.0 + Bootstrap5.0</li>
+          <li>後端：Express.js + Google Firestore (noSQL)</li>
+          <li>MIS：Google Cloud Platform, Google Domain, Google Workspace</li>
+          <li>其它：媒合度演算法、綠界金流</li>
+        </ul>
+      </MyListItem>
+      <MyListItem>
+        <h4>普鴻資訊股份有限公司 前端工程師</h4>
+        <h5>October 2020 ~</h5>
+        <ul>
+          <li>駐點知名人壽開發保單審核系統，團隊規模約15人</li>
+          <li>系統性分享專案經驗</li>
+          <li>
+            <a href="https://www.flyingfar.net/node/170">誠遠國際財商桌遊講師</a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/hashtag/%E5%88%86%E4%BA%AB%E8%80%85%E5%9F%B9%E8%A8%93%E7%8F%AD/?__gid__=304681766894402"
+              target="_blank">分享者培訓班講者</a>
+          </li>
+        </ul>
+      </MyListItem>
+      <MyListItem>
+        <h4>某三公司 網頁工程師</h4>
+        <h5>March 2018 - July 2020</h5>
+        <ul>
+          <li>
+            用Vue開發網站
+          </li>
+          <li>
+            <a href="https://www.sce.pccu.edu.tw/courses/0IZCsample">
+              中國文化大學進修部兼任講師 (未開課)
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/learnspeak.tw/posts/1158473404341326" target="_blank">
+              學說專題演講
+            </a>
+          </li>
+        </ul>
+      </MyListItem>
+      <!-- <MyListItem>
+        <h4>香蕉會兒設計有限公司 遊戲工程師</h4>
+        <h5>March 2017 - March 2018</h5>
+        <ul>
+          <li>感謝Rita容忍我的不成熟</li>
+          <li>相比後面真的沒什麼成績可以拿出來講XD</li>
+        </ul>
+      </MyListItem> -->
+      <MyListItem>
+        <h4>工作以前</h4>
+        <h5>June 2009 - March 2017</h5>
+        <ul>
+          <li>
+            <a href="https://drive.google.com/drive/folders/0B0eqIVhCIJL2ZEFWVXIxYkxCRjg?usp=sharing" target="_blank"
+              rel="noopener">美國東北大學Informatics碩士</a>
+          </li>
+          <li>義務役政戰預官</li>
+          <li>國立中興大學資工系</li>
+        </ul>
+      </MyListItem>
+    </section>
+    <hr />
+    <section class="mt-3">
+      <h2>Thank list</h2>
+      <p>
+        Logo made by Charlie from
+        <a rel="noreferrer" href="https://www.branding-good.com" target="_blank">好日子視覺設計</a>
+      </p>
+      <p>
+        Team support and materials from
+        <a rel="noreferrer" href="https://www.azsinopro.com.tw/" target="_blank">azsinopro</a>
+      </p>
+      <p>
+        Icons made by
+        <a rel="noreferrer" href="https://www.flaticon.com/authors/freepik" title="Freepik" target="_blank">Freepik</a>
+        from
+        <a rel="noreferrer" href="https://www.flaticon.com/" title="Flaticon" target="_blank">
+          www.flaticon.com</a>
+      </p>
+    </section>
   </div>
 </template>
 <script>
